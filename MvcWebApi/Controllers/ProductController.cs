cf0b4163663abe9545c51d35bec18186ab82cb94
Joseph McIntyre <jwm280@gmail.com>
2015-12-24 11:09:38 -0500

    Got some mvc stuff working

---
+++ MvcWebApi/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MvcWebApi.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            products.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new {id = item.Id});
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+    }
+}
